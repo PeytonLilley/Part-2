b4a8feb49eeb87ea57627a592b71e8721ed958c3
lilleye <lilleye@sheridancollege.ca>
2024-02-21 11:00:27 -0500

    Task 2 complete

--- Assets/Week 7/Scripts/Controller.cs
+++ Assets/Week 7/Scripts/Controller.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public Slider chargeSlider;
+    float charge;
+    public float maxCharge = 1;
+    Vector2 direction;
     public static Player SelectedPlayer { get; private set; }
     public static void SetSelectedPlayer(Player player)
     {
@@ -13,7 +17,42 @@ public class Controller : MonoBehaviour
         {
             SelectedPlayer.Selected(false);
         }
+        //
         player.Selected(true);
+        SelectedPlayer = player;
     }
-    
+
+    private void FixedUpdate()
+    {
+        if (direction != Vector2.zero)
+        {
+            SelectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            charge = 0;
+            chargeSlider.value = charge;
+        }
+    }
+
+    private void Update()
+    {
+        if (SelectedPlayer == null) return;
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Space))  //no charge, no direction
+        {
+            charge = 0;
+            Mathf.Clamp(charge, 0, maxCharge);
+            direction = Vector2.zero;
+        }
+        if (Input.GetKey(KeyCode.Space))  //increment charge, put it in the slider
+        {
+            charge += Time.deltaTime;
+            chargeSlider.value = charge;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))  //get the direction, multiply by the charge value
+        {
+            direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - SelectedPlayer.transform.position).normalized * charge;
+        }
+    }
 }
--- Assets/Week 7/Scripts/Player.cs
+++ Assets/Week 7/Scripts/Player.cs
@@ -8,13 +8,16 @@ public class Player : MonoBehaviour
 {
     
     SpriteRenderer sr;
+    Rigidbody2D rb;
     public Color selectedColor;
     public Color unselectedColor;
+    public float speed = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent <SpriteRenderer>();
+        rb = GetComponent <Rigidbody2D>();
         Selected(false);
     }
 
@@ -39,5 +42,9 @@ public class Player : MonoBehaviour
         {
             sr.color = unselectedColor;
         }
+    }
+    public void Move(Vector2 direction) 
+    {
+        rb.AddForce(direction * speed, ForceMode2D.Impulse);
     }
 }
